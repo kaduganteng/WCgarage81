6ddab38e1e83eb3bdb5b961e6a4fb2d766630382
kaduganteng <rayhanjundi77@gmail.com>
2021-11-13 11:35:32 +0700

    First again commit

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'superadmin',
+            'email' => 'rayhanjundi77@gmail.com',
+            'password' => Hash::make('admin123'),
+            'role_id' => 1
+       ]);
+
+
+       DB::table('users')->insert([
+        'name' => 'Admin1',
+        'email' => 'rayhanjundi3@gmail.com',
+        'password' => Hash::make('administrator'),
+        'role_id' => 2
+       ]);
+
+   
+    }
+}
