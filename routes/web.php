2e332c96a55b88a5f0080cf492b3cde4b6ea84c6
kaduganteng <rayhanjundi77@gmail.com>
2021-11-14 02:49:22 +0000

    satu

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/test',function(){return "hello"; })->middleware('SuperAdmin');
+    Route::get('/test',function(){return "hello"; })->middleware('SuperAdmin');
