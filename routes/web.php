1322ee686049519fb13dc7deb0776da6b184df3b
kaduganteng <rayhanjundi77@gmail.com>
2021-11-22 21:04:52 +0700

    selesai crud menu , tambah item delete , item update masi eror2

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,18 @@ Auth::routes();
 // Route::get('/home', 'HomeController@index')->name('home');
 // Route::get('/test',function(){return "hello"; })->middleware('SuperAdmin');
 
-// Route BackEnd//
+// Route BackEnd  //
 
 Route::get('/admin', 'DashboardController@index')->name('dashboard');
+
+//  Kategori menu
 
 Route::get('/kategorimenu', 'KategoriMenuController@index')->name('kategorimenu');
 Route::get('/kategor.create', 'KategoriMenuController@create')->name('kategorimenu.create');
 Route::post('/kategor.store', 'KategoriMenuController@store')->name('kategorimenu.store');
 Route::get('/kategor.delete.{id}', 'KategoriMenuController@destroy')->name('kategorimenu.destroy');
 
-
-
+//  Menu Bandung
 
 Route::get('/caffe', 'CaffeController@ds')->name('dscaffe');
 Route::get('/menubdg', 'CaffeController@index')->name('menubdg');
@@ -37,15 +38,16 @@ Route::post('/menubdg.store', 'CaffeController@store')->name('menubdg.store');
 Route::get('/menubdg.delete.{id}', 'CaffeController@destroy')->name('menubdg.destroy');
 Route::post('/menubdg.update.{id}', 'CaffeController@edit')->name('menubdg.update');
 
-
-
+//  Menu Cimahi
 
 Route::get('/menucmh', 'CaffeController@index2')->name('menucmh');
 
+//  Frenchaise
 
 Route::get('/fcaise', 'FrenchaiseController@ds')->name('fcaise');
 Route::get('/menufcaise', 'FrenchaiseController@index')->name('menufcaise');
 
+//  Route Statistik
 
 Route::get('/statistik', 'StatistikController@index')->name('statistik');
 
