3d7710df72a2a755cfba6e0e02353e9788a87977
kaduganteng <rayhanjundi77@gmail.com>
2021-12-08 17:18:10 +0700

    koneksi BE ke FE  menu di cafe bandung 50%

--- resources/views/cafe81/index.blade.php
+++ resources/views/cafe81/index.blade.php
@@ -1,35 +1,68 @@
 @extends('layouts.app')
 @section('section')
-<header id="fh5co-header" class="fh5co-cover js-fullheight" role="banner" style="background-image: url(images/hero_1.jpeg);" data-stellar-background-ratio="0.5">
-    <div class="overlay"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <div class="display-t js-fullheight">
-                    <div class="display-tc js-fullheight animate-box" data-animate-effect="fadeIn">
-                        <h1>Our Product</h1>
-                        <div class="h2tagline">
-                        <h2>Cafe</h2>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+
+
+    <header id="fh5co-header" class="fh5co-cover js-fullheight" role="banner"
+        style="background-image: url(images/hero_1.jpeg);" data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="display-t js-fullheight">
+                        <div class="display-tc js-fullheight animate-box" data-animate-effect="fadeIn">
+                            <h1>Our Product</h1>
+                            <div class="h2tagline">
+                                <h2>Cafe</h2>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</header>
-<body>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <div class="card" style="width: 25rem;">
-                    <img src="../images/nkg.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </header>
+
+    <body>
+        <div class="container">
+            <div id="fh5co-featured-menu" class="fh5co-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 fh5co-heading animate-box">
+                            <h2>Our Delicous Menu</h2>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ab debitis sit
+                                        itaque totam, a maiores nihil, nulla magnam porro minima officiis! Doloribus aliquam
+                                        voluptates corporis et tempora consequuntur ipsam, itaque, nesciunt similique
+                                        commodi omnis. Ad magni perspiciatis, voluptatum repellat.</p>
+                                </div>
+                            </div>
+                        </div>
+                        @foreach ($menubdg as $m)
+                            <div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap">
+                                <div class="fh5co-item animate-box">
+
+                                    <a href="{{ asset('upload/' . $m->foto_menu) }}" class="fancybox"
+                                        data-fancybox="ggblg" data-gallery="gallery" height="50px" width="50px">
+                                        <img src="{{ asset('upload/' . $m->foto_menu) }}" class="img-fluid mb-2"
+                                            alt="white sample" width="100px" height="100px" />
+                                        <h3>{{ $m->nama }}</h3>
+                                        <span class="fh5co-price">{{ $m->harga }}</sup></span>
+                                        <p>{!! $m->keterangan !!}</p>
+
+                                </div>
+
+                            </div>
+                        @endforeach
+
+
                     </div>
-                  </div>
-            <div class="col-md-4"></div>
-            <div class="col-md-4"></div>
+                </div>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
 @endsection
